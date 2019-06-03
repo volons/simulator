2016819199b386214c3a5ad0a86325062ee959cd
Artkub <artkub.artkub@gmail.com>
2019-06-03 16:07:55 +0200

    add run.sh

---
+++ files/run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ -z "$DOCKER_HOST_IP" ]
+then
+	DOCKER_HOST_IP_LINUX=$(ip route|awk '/default/ { print $3 }')
+	DOCKER_HOST_IP_MAC=$(dig +short host.docker.internal)
+	DOCKER_HOST_IP=${DOCKER_HOST_IP_MAC:-$DOCKER_HOST_IP_LINUX}
+fi
+
+/usr/local/bin/dronekit-sitl copter-3.3 &
+mavproxy.py --master tcp:127.0.0.1:5760 --sitl 127.0.0.1:5501 --out ${DOCKER_HOST_IP}:14550 --out vehicle-mavlink:14590
